cf64ba70424dd43c90482c7807d46a252664f5ee
malikobansa <hackobansa@gmail.com>
2023-09-21 17:56:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
